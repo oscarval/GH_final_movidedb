255a644060f4e397eaa13c746bb89636f6113ee5
Alexis <entulinea.alexis@gmail.com>
2020-06-03 01:55:41 +0200

    Reorganize project. Create necessary components Add dependencies: redux, axios, react-thunk, node-sass,  redux-localstorage-simp`le

---
+++ src/services/redux/store.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./reducer.js";
+import { save, load } from "redux-localstorage-simple";
+const createStoreWithMiddleware = applyMiddleware(save())(createStore);
+
+const store = createStoreWithMiddleware(
+  reducer,
+  load(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
