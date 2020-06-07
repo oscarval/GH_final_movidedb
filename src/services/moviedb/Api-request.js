d7642b2c2d936a161c94ff67c135989163c0fcef
Alexis <entulinea.alexis@gmail.com>
2020-06-07 22:08:34 +0200

    Apirequest for movies and series

--- src/services/moviedb/Api-request.js
+++ src/services/moviedb/Api-request.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 import Config from "../config/config";
 
+/**
+ * ApiRequest Services
+ * Movies functionality
+ * Series funtionality
+ */
 const ApiRequest = {
   Movies: {
     Search: (paramSearch) => {
       return async (dispatch) => {
         console.log("entra dispatch. Loading");
         try {
-          const moviesResponse = await axios
-            .get(`${Config.ApiRequest.credentials.baseURL}/search/movie`, {
-              params: {
-                ...Config.ApiRequest.credentials.defaultParameters,
-                query: paramSearch,
-              },
-            })
-            .then((res) => res.data);
+          const moviesResponse = await get("movie", paramSearch).then(
+            (res) => res.data
+          );
           dispatch({
             type: Config.ApiRequest.actionsTypes.SEARCH_MOVIES,
             payload: moviesResponse,
@@ -35,6 +35,46 @@ const ApiRequest = {
       };
     },
   },
+  Series: {
+    Search: (paramSearch) => {
+      return async (dispatch) => {
+        console.log("entra dispatch. Loading");
+        try {
+          const seriesResponse = await get("tv", paramSearch).then(
+            (res) => res.data
+          );
+          dispatch({
+            type: Config.ApiRequest.actionsTypes.SEARCH_SERIES,
+            payload: seriesResponse,
+          });
+        } catch (err) {
+          dispatch({
+            type: Config.ApiRequest.actionsTypes.ERROR_SEARCH,
+            payload: [
+              {
+                code: -1,
+                message: err.message,
+              },
+            ],
+          });
+        } finally {
+          console.log("hide loading");
+        }
+      };
+    },
+  },
+};
+
+const get = async (searchCategory, paramSearch) => {
+  const response = await axios
+    .get(`${Config.ApiRequest.credentials.baseURL}/search/${searchCategory}`, {
+      params: {
+        ...Config.ApiRequest.credentials.defaultParameters,
+        query: paramSearch,
+      },
+    })
+    .then((res) => res.data);
+  return response;
 };
 
 export default ApiRequest;
